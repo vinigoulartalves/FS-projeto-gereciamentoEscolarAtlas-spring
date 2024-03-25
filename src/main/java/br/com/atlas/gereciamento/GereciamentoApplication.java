95fb5de3f1e301a641e865d4267ee0aa6580b6c0
Vinicius Goulart Alves <vini.goulartalves@gmail.com>
2024-03-25 19:39:40 -0300

    Commit inicial - Criação do repositório

---
+++ src/main/java/br/com/atlas/gereciamento/GereciamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.atlas.gereciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GereciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GereciamentoApplication.class, args);
+	}
+
+}
